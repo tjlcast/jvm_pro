70edd1ed17917ccbb397f67440f8382ca2bb314a
tangjialiang <phx108@sina.com>
2019-05-07 15:04:39 +0800

    new files.

--- classfile/class_file.go
+++ classfile/class_file.go
@@ -1,9 +1,11 @@
 package classfile
 
+import "fmt"
+
 type ClassFile struct {
 	// magic				uint32
 	minorVersion 			uint16
-	magorVersion			uint16
+	majorVersion			uint16
 	constantPool			ConstantPool
 	accessFlags				uint16
 	thisClass				uint16
@@ -14,15 +16,129 @@ type ClassFile struct {
 	attributes				[]AttributeInfo
 }
 
+/**
+	Parse() 函数把[]byte解析成ClassFile结构体
+ */
 func Parse(classData []byte) (cf *ClassFile, err error) {
-	// todo
-	return nil, nil
+	defer func() {
+		if r := recover(); r != nil {
+			var ok bool
+			err, ok = r.(error)
+			if !ok {
+				err = fmt.Errorf("%v", r)
+			}
+		}
+	} ()
+
+	cr := &ClassReader{classData}
+	cf = &ClassFile{}
+	cf.read(cr)
+
+	return
 }
 
 func (self *ClassFile) read(reader *ClassReader) {
+	self.readAndCheckMagic(reader)
+
+	self.readAndCheckVersion(reader)
+
+	self.constantPool = readConstantPool(reader)
+
+	self.accessFlags = reader.readUint16()
+
+	self.thisClass = reader.readUint16()
+
+	self.superClass = reader.readUint16()
+
+	self.interfaces = reader.readUint16s()
+
+	self.fields = readMembers(reader, self.constantPool)
+
+	self.methods = readMembers(reader, self.constantPool)
+
+	self.attributes = readAttributes(reader, self.constantPool)
+}
+
+/**
+	魔术
+ */
+func (self *ClassFile) readAndCheckMagic(reader *ClassReader) {
+	magic := reader.readUint32()
+	if magic != 0xCAFEBABE {
+		panic("java.lang.ClassFormatError: magic!")
+	}
+}
+
+/**
+	版本号
+ */
+func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {
+	self.minorVersion = reader.readUint16()
+	self.majorVersion = reader.readUint16()
+
+	switch self.majorVersion {
+	case 45:
+		return
+	case 46, 47, 48, 49, 50, 51, 52:
+		if self.minorVersion == 0 {
+			return
+		}
+	}
+
+	panic("java.lang.UnsupportedClassVersionError!")
+}
+
+func (self *ClassFile) MinorVersion() uint16 {
+	return self.minorVersion
+}
+
+func (self *ClassFile) MajorVersion() uint16 {
+	return self.majorVersion
+}
+
+func (self *ClassFile) ConstantPool() ConstantPool {
 	// todo
 }
 
-func (self *ClassFile) readAndCheckMagic(reader *ClassReader) {
+/**
+	类访问标志
+ */
+func (self *ClassFile) AccessFlag() uint16 {
 	// todo
+}
+
+func (self *ClassFile) Fields() []*MemberInfo {
+	// todo
+}
+
+func (self *ClassFile) Methods() []*MemberInfo {
+	// todo
+}
+
+/**
+	类索引
+ */
+func (self *ClassFile) ClassName() string {
+	return self.constantPool.getClassName(self.thisClass)
+}
+
+/**
+	超类索引
+ */
+func (self *ClassFile) SuperClassName() string {
+	if self.superClass > 0 {
+		return self.constantPool.getClassName(self.superClass)
+	}
+	return "" // 只有 java.lang.Object 没有超类
+}
+
+/**
+	接口索引表
+ */
+func (self *ClassFile) InterfaceNames() []string {
+	interfaceNames := make([]string, len(self.interfaces))
+	for i, cpIndex := range self.interfaces {
+		interfaceNames[i] = self.constantPool.getClassName(cpIndex)
+	}
+	return interfaceNames
 }
---
+++ classfile/constant_pool.go
@@ -0,0 +1,59 @@
+package classfile
+
+type ConstantPool []ConstantInfo
+
+func readConstantPool(reader *ClassReader) ConstantPool {
+	cpCount := int(reader.readUint16())
+	cp := make([]ConstantInfo, cpCount)
+
+	for i:=1; i < cpCount; i++ {
+		cp[i] = readConstantInfo(reader, cp)
+		switch cp[i].(type) {
+		case *ConstantLongInfo, *ConstantDoubleInfo:
+			i++
+		}
+	}
+	return cp
+}
+
+func (self ConstantPool) getConstantInfo(index uint16) ConstantInfo {
+	if cpInfo := self[index]; cpInfo != nil {
+		return cpInfo
+	}
+	panic("Invalid constant pool index!")
+}
+
+func (self ConstantPool) getNamedAndType(index uint16) (string, string) {
+	ntInfo := self.getConstantInfo(index).(*ConstantNameAndTypeInfo)
+	name := self.getUtf8(ntInfo.nameIndex)
+	_type := self.getUtf8(ntInfo.descriptorIndex)
+	return name, _type
+}
+
+func (self ConstantPool) getClassName(index uint16) string {
+	classInfo := self.getConstantInfo(index).(*ConstantNameAndTypeInfo)
+	return self.getUtf8(classInfo.nameIndex)
+}
+
+func (self ConstantPool) getUtf8(index uint16) string {
+	utf8Info := self.getConstantInfo(index).(*ConstantNameAndTypeInfo)
+	return utf8Info.str
+}
+
+
+/**
+		以下是常量池的项定义
+
+ */
+
+type ConstantInfo interface {
+	readInfo(reader *ClassReader)
+}
+
+func readConstantInfo(reader *ClassReader, cp ConstantPool) ConstantInfo {
+	// todo
+}
+
+func newConstantInfo(tag uint8, cp ConstantPool) ConstantInfo {
+	// todo
+}
